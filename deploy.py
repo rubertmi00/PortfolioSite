928b2034843ac7ef7089aa8f6bccb65ff8fe9b08
Michael Ruberto <rubertmi@pascack.org>
2019-02-06 10:35:02 -0500

    Capitalize Procfile so it works. Thanks for nothing, Tom.

--- deploy.py
+++ deploy.py
@@ -1,21 +1,26 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
    return render_template("index.html")
 
+
 @app.route("/programming")
 def programming():
    return render_template("programming.html")
+
 
 @app.route("/extracurriculars")
 def extracurriculars():
    return render_template("extracurriculars.html")
 
+
 @app.route("/social")
 def social():
    return render_template("social.html")
+
 
 @app.route("/contact")
 def contact():
